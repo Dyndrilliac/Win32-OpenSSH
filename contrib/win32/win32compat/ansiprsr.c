3d38805276ffbf60b2f51e18c8278065f934ee76
Ray Hayes <rayhayes@rhbe.net>
2016-09-20 11:11:06 -0700

    Minor fixes for color handling and newline handling.

--- contrib/win32/win32compat/ansiprsr.c
+++ contrib/win32/win32compat/ansiprsr.c
@@ -250,8 +250,8 @@ unsigned char* ParseBuffer(unsigned char* pszBuffer, unsigned char* pszBufferEnd
 		case 10:
 			pszBuffer++;
 			AutoWrap = 1;
-		    bAtEOLN = TRUE;
-		    break;
+            GoToNextLine();
+            break;
 				
 		case 12:
 		    pszBuffer++;
@@ -869,7 +869,7 @@ unsigned char * ParseANSI(unsigned char * pszBuffer, unsigned char * pszBufferEn
 		bCS1 = 0;
 		bBkMode = 0;
 		bCharMode = 0;
-		return pszCurrent;
+        return pszCurrent;
 	}
 	else
 		return pszBuffer;
@@ -912,9 +912,9 @@ unsigned char * ParseVT52(unsigned char * pszBuffer, unsigned char * pszBufferEn
 			break;
 
 		case 'H':  // Cursor Home
-			ConSetCursorPosition(1, 1);
+			ConSetCursorPosition(0, 0);
 			pszCurrent++;
-				bAtEOLN = FALSE;
+			bAtEOLN = FALSE;
 			break;
 		case 'I':  // Reverse Line Feed
 			pszCurrent++;
